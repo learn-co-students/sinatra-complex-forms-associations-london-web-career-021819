4bc98d7620014a35f5c6f2d304ee91ee7718314c
Alex Bujenita <dekc89@gmail.com>
2019-03-13 12:07:34 +0000

    done

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,15 +2,27 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
+    # binding.pry
     erb :'/pets/index' 
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all
+    # binding.pry
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
+    @pet = Pet.create(params['pet'])
+    # binding.pry
+    if !params["owner_name"]["name"].empty?
+      # binding.pry
+      @owner = Owner.create(name: params["owner_name"]["name"])
+      # binding.pry
+      @owner.pets << @pet
+      # binding.pry
+    end
+    # binding.pry
     redirect to "pets/#{@pet.id}"
   end
 
@@ -19,8 +31,26 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :'/pets/edit'
+  end
 
+  patch '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    # binding.pry
+    @pet.update(params['pet'])
+    # binding.pry
+    if !params['owner']['name'].empty?
+      # binding.pry
+      @owner = Owner.create(name: params['owner']['name'])
+      # binding.pry
+      @owner.pets << @pet
+      # binding.pry
+    end
+    # binding.pry
+    # "asd"
     redirect to "pets/#{@pet.id}"
   end
 end
